325ca48d42fb6cc6846358961ec0794e7f58457f
James Rising <jarising@gmail.com>
2017-05-02 23:22:00 -0700

    count unique vars

---
+++ analysis/countunique.py
@@ -0,0 +1,24 @@
+import hashlib, importlib
+
+allvars = {} # hash: [variables]
+for modname in ['AHRF', 'USGS', 'acra', 'agriculture', 'census', 'crime', 'election', 'groundwater']:
+    print modname
+    main = importlib.import_module("%s.main" % modname)
+    db = main.load()
+    for variable in db.get_variables():
+        years = db.get_years(variable)
+        if years is None:
+            years = ['']
+
+        for year in years:
+            name = "%s.%s:%s" % (modname, variable, year)
+            data = db.get_data(variable, year)
+
+            myhash = hashlib.sha224(str(data))
+            if myhash in allvars:
+                print variable, year, "same as", allvars[myhash][0]
+                allvars[myhash].append(name)
+            else:
+                allvars[myhash] = [name]
+
+print "Total count: ", len(allvars)
