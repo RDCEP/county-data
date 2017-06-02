8d30105736fff3181338dc5ae2c7528e85ad96e3
James Rising <jarising@gmail.com>
2017-06-02 10:32:02 -0400

    exporting and describing

--- export.py
+++ export.py
@@ -1,16 +1,25 @@
 import os, importlib, csv
 from cmd import Cmd
+import numpy as np
 
 class Exporter(Cmd):
+    prompt = "> "
+
     loaded = {} # prefix: db
     variables = []
-    
+
     def do_available(self, line):
+        """List all available databases.
+        Usage: available
+        """
         for root, dir, files in os.walk("."):
             if '__init__.py' in files:
                 print root[2:]
 
     def do_load(self, module):
+        """Load an available database.
+        Usage: available [DB name]
+        """
         print "Loading " + module + "..."
         try:
             db = importlib.import_module(module + '.main').load()
@@ -19,8 +28,23 @@ class Exporter(Cmd):
         except Exception as ex:
             print "Failed to load " + module
             print ex
-            
+
+    def do_describe(self, variable):
+        """Describe a variable.
+        Usage: describe [variable]
+        """
+        try:
+            # Look for this variable
+            db = self.get_database()
+            print db.describe_variable(variable)
+            print "Years: " + str(db.get_years(variable))
+        except:
+            print "Could not find variable " + variable
+
     def do_list(self, line):
+        """List the available variables across all loaded databases.
+        Usage: list
+        """
         db = self.get_database()
         if db is None:
             print "None."
@@ -28,25 +52,32 @@ class Exporter(Cmd):
             print db.get_variables()
 
     def do_add(self, variable):
+        """Add a variable to the export list.
+        Usage: add [variable]
+        """
         try:
             # Look for this variable
-            print self.get_database().describe_variable(variable)
-            print "Years: " + str(variable.get_years(variable))
+            db = self.get_database()
+            print db.describe_variable(variable)
+            print "Years: " + str(db.get_years(variable))
             self.variables.append(variable)
         except:
             print "Could not find variable " + variable
-        
+
     def do_export(self, filepath):
+        """Export all variables on the export list to a file.
+        Usage: export [file path]
+        """
         # Collect all data
         data = {}
         db = self.get_database()
         for variable in self.variables:
             years = db.get_years(variable)
             if years is None:
-                data[variable] = db.get_data(variable, None)
+                data[variable] = np.array(db.get_data(variable, None))
             else:
                 for year in years:
-                    data[variable + ':' + str(year)] = db.get_data(variable, year)
+                    data[variable + ':' + str(year)] = np.array(db.get_data(variable, year))
 
         with open(filepath, 'w') as fp:
             writer = csv.writer(fp)
@@ -54,7 +85,7 @@ class Exporter(Cmd):
             for ii in range(len(db.get_fips())):
                 row = [db.get_fips()[ii]] + [data[key][ii] for key in data.keys()]
                 writer.writerow(row)
-        
+
     def get_database(self):
         if not self.loaded:
             return None
@@ -65,8 +96,11 @@ class Exporter(Cmd):
         return CombinedDatabase(self.loaded.values(), self.loaded.keys(), '.')
 
     def do_bye(self, line):
-        self.close()
-        bye()
+        """Close the interface.
+        Usage: bye
+        """
         return True
-    
+
+print "Welcome to the County Data Exporter."
+print "For help, type `help`.\n"
 Exporter().cmdloop()
