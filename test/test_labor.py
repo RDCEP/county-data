a99bd281e87dff2e956784b9322d6dd2d77bf4fe
James Rising <jarising@gmail.com>
2017-07-01 16:39:05 -0400

    fix labor test

--- test/test_labor.py
+++ test/test_labor.py
@@ -7,7 +7,7 @@ def db():
 
 def test_load(db):
     allvars = db.get_variables()
-    print sum([sum(db.get_data(var, 2002)) for var in allvars])
+    print sum([sum(db.get_data(var, 2002)) for var in allvars if var != 'fips'])
 
 def test_units(db):
     for variable in db.get_variables():
@@ -17,4 +17,5 @@ if __name__ == '__main__':
     mydb = db()
     for variable in mydb.get_variables():
         print variable, mydb.get_unit(variable)
-
+    test_load(mydb)
+    test_units(mydb)
